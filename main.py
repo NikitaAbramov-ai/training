dcd586b35168bbeb75801f1606f541b8a051d0f6
NikitaAbramov-ai <abramov-nikitka@bk.ru>
2024-09-08 17:42:43 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+                # "1st program"
+print(9 ** 0.5 *5)
+
+                # "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+                # "3rd program"
+a = print(2 * 2 + 2)
+b = print(2 * (2 +2))
+print(a == b)
+
+                # "4th program"
+c = float('123.456')
+d= c * 10
+e = d % 10
+w = print(int(e))
+
