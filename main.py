6a06af7eb21675a0065a50b5c210cb545d12b4dc
NikitaAbramov-ai <abramov-nikitka@bk.ru>
2024-09-10 23:31:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-                # "1st program"
-print(9 ** 0.5 *5)
-
-                # "2nd program"
-print(9.99 > 9.98 and 1000 != 1000.1)
-
-                # "3rd program"
-a = print(2 * 2 + 2)
-b = print(2 * (2 +2))
-print(a == b)
-
-                # "4th program"
-c = float('123.456')
-d= c * 10
-e = d % 10
-w = print(int(e))
-
+a = 'Топинамбур'
+print(a[0])
+print(a[-1])
+print(a[-5::])
+print(a[::-1])
+print(a[1::2])
