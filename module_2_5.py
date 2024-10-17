1f2c0ecb6553b133e5b3e59eac0ad6a5742d84d5
NikitaAbramov-ai <abramov-nikitka@bk.ru>
2024-10-17 12:39:16 +0700

    module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,10 +1,11 @@
 def get_matrix (n,m,value):
     matrix = []
+    if n <= 0 or m <= 0:
+        return matrix
     for i in range(n):
         matrix.append([])
-    for  j in range(m):
-        matrix[i].append(value)
-    print(matrix)
+        for  j in range(m):
+            matrix[i].append(value)
     return matrix
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
