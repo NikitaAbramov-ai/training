d31a33db0742664bca1f377539e09d4f7980cafd
NikitaAbramov-ai <abramov-nikitka@bk.ru>
2024-10-18 20:48:31 +0700

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,5 +1,6 @@
 def send_email(message, recipient, sender = "university.help@gmail.com"):
-    if ("@" and (".com" or ".ru" or ".net")) not in (recipient and sender):
+    if '@' not in recipient or not recipient.endswith(
+            ('.com', '.ru', '.net')) or '@' not in sender or not sender.endswith(('.com', '.ru', '.net')):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif sender == recipient:
         print("Нельзя отправить письмо самому себе!")
