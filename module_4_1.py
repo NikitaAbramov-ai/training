981457cb9ffda4ac9091e9e72fe3419a52c4bd4f
NikitaAbramov-ai <abramov-nikitka@bk.ru>
2024-10-19 22:30:47 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as fake_
+import true_math as true_
+
+result1 = fake_.divide(69, 3)
+result2 = fake_.divide(3, 0)
+result3 = true_.divide(49, 7)
+result4 = true_.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
